31e14835e4fe1eb28959ba2fbe3042688938afe0
Nina Evelina Khodnia <khodnia.ninaevelina@gmail.com>
2023-11-27 22:00:36 +0100

    (docs): updates cors origin

--- fed22s-backend/app.ts
+++ fed22s-backend/app.ts
@@ -17,10 +17,7 @@ app.use((req, res, next) => {
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://react-group-assignment-api.vercel.app/",
-    ],
+    origin: ["https://react-group-assignment-frontend.vercel.app/"],
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );

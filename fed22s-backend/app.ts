dda7300a648f6334dc404c61826f5c0281e0004f
Nina Evelina Khodnia <khodnia.ninaevelina@gmail.com>
2023-11-28 20:07:20 +0100

    tests

--- fed22s-backend/app.ts
+++ fed22s-backend/app.ts
@@ -32,7 +32,7 @@ app.use((req, res) => {
   console.log(req.path);
   console.log(isApiPath);
 
-  if (isApiPath) return res.sendStatus(404);
+  if (isApiPath) return res.sendStatus(200);
 
   return;
 });

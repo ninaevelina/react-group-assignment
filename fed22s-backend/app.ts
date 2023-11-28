8f867519e84a1431ad09e76a996872efab258018
Nina Evelina Khodnia <khodnia.ninaevelina@gmail.com>
2023-11-28 16:15:05 +0100

    adds tests

--- fed22s-backend/app.ts
+++ fed22s-backend/app.ts
@@ -17,7 +17,7 @@ app.use((req, res, next) => {
 
 app.use(
   cors({
-    origin: "https://react-group-assignment-frontend.vercel.app/",
+    origin: "https://react-group-assignment-frontend.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
